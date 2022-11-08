ec228eeaf98fa7624ca18ed6404dc53793185014
Shiti <shitigarg2003@gmail.com>
2022-11-08 18:29:46 +0530

    fetched current crypto data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState,useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [crypto, setcrypto] = useState("");
+  const handleChange=(e)=>{
+    // selectElement = document.querySelector('#select1');
+    // output = selectElement.options[selectElement.selectedIndex].value;
+    // console.log(e.target.value)
+    setcrypto(e.target.value);
+  }
+
+  useEffect(() => {
+    if(crypto!==""){
+      fetch(`https://api.coincap.io/v2/assets/${crypto}`)
+      .then(result=>{return result.json()})
+      .then(data=> console.log(data))
+    }
+  }, [crypto])
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container text-center">
+        <h1 className="my-3">crypto-sell-notifier</h1>
+        <select id="select1" className="form-select" aria-label="Default select example" onChange={handleChange} defaultValue={'DEFAULT'} >
+          <option value="DEFAULT" disabled >Open this select menu</option>
+          <option value="bitcoin">Bitcoin</option>
+          <option value="ethereum">Etherium</option>
+          <option value="dogecoin">DogeCoin</option>
+        </select>
+      </div>
+    </>
   );
 }
 
